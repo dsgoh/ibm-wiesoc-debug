7851afb7c85b665cfb76fb3fc295a9d492f8e9af
Dylan <dylan@ucrg.cc>
2020-10-11 10:36:48 +1100

    Fixing bug in progress of business

--- website/app.py
+++ website/app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for
 import maps
 import covidsafe
 import time
+import venue
+from datetime import datetime
+import json
+import sys
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,15 +18,18 @@ def individual():
         form = request.form
         dest = form.get('DestinationInput')
         datetimein = form.get('DatetimeIndividualInput')
-        print(dest, datetimein)
+        #print(dest, datetimein)
         v = maps.queryVenue(dest)
-        venueOut = v["name"] + " - " + v["formatted_address"]
+        venueOut = v["name"] + ", " + v["formatted_address"]
         venueCOVIDSafeStatus = covidsafe.checkCovidSafe(v["formatted_address"])
         venueName = v["name"]
-        #print(covidsafe.checkCovidSafe(venue))
         route = maps.queryRoute("Sydney City", dest, departure_time="now")
+        if route is None:
+            route = "No value returned for route."
+        venueForecast = venue.forecastVenue(venueOut, datetimein)
+
         #route = maps.queryRoute("Sydney City", dest, departure_time=int(time.mktime(time.strptime(datetimein, '%Y-%m-%dT%H:%M'))))
-        return redirect(url_for('individual_search', dest=dest, datetimein=datetimein, venueOut=venueOut, venueCOVIDSafeStatus=venueCOVIDSafeStatus, route=route))
+        return redirect(url_for('individual_search', dest=dest, datetimein=datetimein, venueOut=venueOut, venueCOVIDSafeStatus=venueCOVIDSafeStatus, route=route, venueForecast=venueForecast))
     return render_template('individual.html')
 
 @app.route('/individual/search/', methods=["GET", "POST"])
@@ -32,7 +39,52 @@ def individual_search():
     venueOut = request.args.get('venueOut', None)
     venueCOVIDSafeStatus = request.args.get('venueCOVIDSafeStatus', None)
     route = request.args.get('route', None)
-    return render_template('individual_search.html', dest=dest, datetimein=datetimein, venueOut=venueOut, venueCOVIDSafeStatus=venueCOVIDSafeStatus, route=route)
+    venueForecast = request.args.get('venueForecast', None).replace("\'", '\"')
+    #print("VF: ", venueForecast)
+    newJson = json.loads(venueForecast)
+    print(newJson.keys())
+    busyVenueStatus = 0
+    busyVenueData = 0
+    datetimeobject = datetime.strptime(datetimein, '%Y-%m-%dT%H:%M')
+    if 'analysis' in newJson.keys():
+        for something in newJson['analysis']:
+            if something["day_info"]["day_int"] == datetimeobject.weekday():
+                venueForecast = something
+                flag = False
+                #print(datetimeobject.hour, something['peak_hours'][0]['peak_start'], something['peak_hours'][0]['peak_end'])
+                try:
+                    if datetimeobject.hour in something['quiet_hours']:
+                        busyVenueStatus = 0
+                        flag = True
+                except:
+                    continue
+                try:
+                    if datetimeobject.hour in something['busy_hours']:
+                        busyVenueStatus = 1
+                        flag = True
+                except:
+                    continue
+                try:
+                    if (int(datetimeobject.hour) >= int(something['peak_hours'][0]['peak_start'])) and (int(datetimeobject.hour) <= int(something['peak_hours'][0]['peak_end'])):
+                        busyVenueStatus = 2
+                        flag = True
+                except:
+                    continue
+                try:
+                    if datetimeobject.hour == something['surge_hours']['most_people_come']:
+                        busyVenueStatus = 3
+                        flag = True
+                except:
+                    continue
+                
+                if (flag):
+                    busyVenueData = something
+                print("OUT!")
+    else:
+        busyVenueStatus = 0
+        busyVenueData = 0
+                        
+    return render_template('individual_search.html', dest=dest, datetimein=datetimein, venueOut=venueOut, venueCOVIDSafeStatus=venueCOVIDSafeStatus, route=route, venueForecast=venueForecast, busyVenueStatus=busyVenueStatus, busyVenueData=busyVenueData)
 
 @app.route('/group/')
 def group():
@@ -40,3 +92,5 @@ def group():
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
+
---
+++ website/venue.py
@@ -0,0 +1,35 @@
+import datetime
+import requests
+import json
+
+import maps
+
+BESTTIME_KEY = 'pri_1db2d347883b45f2a98961c75a4221ae'
+
+def checkAuthorisation():
+    """
+    Checks the API key and returns details about it
+    """
+    payload = {}
+    headers= {}
+    response = requests.request("GET", 'https://BestTime.app/api/v1/keys/' + BESTTIME_KEY, headers=headers, data = payload)
+    print(response.text.encode('utf8'))
+
+def forecastVenue(queryString, time):
+    """
+    Query the venue. Makes a call to googlemaps API to get formal address
+    """
+    params = {
+        'api_key_private': BESTTIME_KEY,
+        'venue_name': queryString,
+        'venue_address': maps.queryVenue(queryString)
+    }
+    response = requests.request("POST", 'https://besttime.app/api/v1/forecasts', params=params)
+    data = json.loads(response.text)
+    print(data)
+    return data
+
+# def extractJSON():
+
+if __name__ == '__main__':
+    forecastVenue("Mcdonalds George St Sydney", datetime.datetime.now())
