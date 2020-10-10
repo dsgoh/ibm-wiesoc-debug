56f6361dc2bb044ce3cc740ed9e5b29b50adaaed
dan <dennuguyen@gmail.com>
2020-10-10 15:16:26 +1100

    Unpack dataJSON

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ es = Elasticsearch(
 
 def COVIDSafeBusinesses_metadata(timestamp, Business_reported_name, Business_reported_industry,	Business_reported_address, Business_reported_street_address, Business_reported_suburb, Business_reported_state, Business_reported_country, Business_reported_postcode, Business_standardised_address, Business_standardised_street_address, Business_standardised_Suburb, Business_standardised_State, Business_standardised_Postcode, Longitude, Latitude):
     metadata = {
-        "timestamp" : timestamp.timestamp() * 1000,
+        "timestamp" : timestamp.isoformat(),
         "Business_reported_name" : Business_reported_name,
         "Business_reported_industry" : Business_reported_industry,
         "Business_reported_address" : Business_reported_address,	
@@ -30,14 +30,17 @@ def COVIDSafeBusinesses_metadata(timestamp, Business_reported_name, Business_rep
         "Longitude" : Longitude,	
         "Latitude" : Latitude
     }
+    print(metadata)
     return metadata
 
 indexCOVIDSafeBusinesses = "datansw-covidsafe-businesses-indexv1.0"
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://data.nsw.gov.au/data/api/3/action/datastore_search?resource_id=4a26e0f0-71e1-43bb-96a8-e1434bbce9d8') as url:
-        dataJSON = json.load(url)
-    print(dataJSON)
+        dataJSON = json.load(url) # dataJSON is a nested dict
+        for business in list(dataJSON["result"]["records"]):
+            COVIDSafeBusinesses_metadata(**{'timestamp': datetime.datetime.now(), **business})
+
     time.sleep(1)
     '''
     while True:
